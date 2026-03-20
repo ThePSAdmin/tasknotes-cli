97c7c4ce8d0927a21132a265dd7a284c58cd687a
Anthony Allen <losthammer@gmail.com>
2026-03-20 09:00:39 +0000

    fix: set correct name

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("tasknotes %s (%s)\n", Version, CommitSHA)
+		fmt.Printf("tasknotes-cli %s (%s)\n", Version, CommitSHA)
 	},
 }
 
